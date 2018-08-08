b512c4bdf807bb7f12ab74f0477f1503675c5d57
Christopher Brown <io@henrian.com>
2018-08-08 12:09:58 -0500

    Implement option to trim trailing whitespace

--- nbfilter/clean.py
+++ nbfilter/clean.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import nbformat
 
@@ -17,7 +18,15 @@ def _clear(d, *keys):
                 d[key] = None
 
 
-def clean_notebook(notebook):
+def trim_trailing_whitespace(source):
+    '''
+    Trim whitespace from end of lines in `source`
+    '''
+    # \s is too greedy: it might capture newlines despite re.M flag
+    return re.sub(r'[ \t]+$', '', source, flags=re.MULTILINE)
+
+
+def clean_notebook(notebook, trim=False):
     '''
     Clean all output from `notebook`, which is a
     nbformat.notebooknode.NotebookNode (https://git.io/NotebookNode)
@@ -34,14 +43,18 @@ def clean_notebook(notebook):
             _delete(cell, 'prompt_number')
             if 'metadata' in cell:
                 _delete(cell['metadata'], 'collapsed', 'scrolled')
+            if trim:
+                # the 'source' field is a list of strings in the JSON serialization,
+                # but it's parsed into a single string by nbformat.read
+                cell['source'] = trim_trailing_whitespace(cell['source'])
 
 
-def clean_file(fp_in, fp_out):
+def clean_file(fp_in, fp_out, trim=False):
     '''
     Clean all output from a file, writing the result to fp_out.
     '''
     notebook = nbformat.read(fp_in, nbformat.NO_CONVERT)
-    clean_notebook(notebook)
+    clean_notebook(notebook, trim)
     nbformat.write(notebook, fp_out, nbformat.NO_CONVERT)
 
 
@@ -53,6 +66,9 @@ def main():
     parser.add_argument('-v', '--version',
                         action='version',
                         version=__version__)
+    parser.add_argument('-t', '--trim',
+                        action='store_true',
+                        help='trim trailing whitespace')
     parser.add_argument('-i', '--input',
                         help='input file (default: /dev/stdin)',
                         type=argparse.FileType('r'),
@@ -62,7 +78,7 @@ def main():
                         type=argparse.FileType('w'),
                         default=sys.stdout)
     opts = parser.parse_args()
-    clean_file(opts.input, opts.output)
+    clean_file(opts.input, opts.output, opts.trim)
 
 
 if __name__ == '__main__':
